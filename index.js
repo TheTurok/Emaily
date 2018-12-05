3b09abe4412aa93e5b2ab339fc23722a6bbc85f7
Timothy Choi <TurokChoi@gmail.com>
2018-12-04 19:46:39 -0800

    another thing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'}); //route handler in expreses
+  res.send({bye: 'buddy'}); //route handler in expreses
 });
 
 const PORT = process.env.PORT || 5000
