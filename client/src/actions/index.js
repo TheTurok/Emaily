b3b3f1979b98a1cce6f3ca8f756662214caa1461
Timothy Choi <TurokChoi@gmail.com>
2018-12-15 17:59:47 -0800

    added login and log out more auth verifcation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import {FETCH_USER} from './types';
 
 
-//action creators
+//action creator
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user');
-    dispatch({ type: FETCH_USER, payload: res });
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+  return(
+    <div style = {{textAlign: 'center'}}>
+      <h3>
+        Collect feedback from you users
+      </h3>
+    </div>
+  );
+}
+
+export default Landing;
