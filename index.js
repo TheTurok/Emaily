cf9a5e46ac959a832c030f38692f32d496c56d2d
Timothy Choi <TurokChoi@gmail.com>
2018-12-31 15:21:10 -0800

    heroku prod

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); //google authentication
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production'){
+  //express will serve up production assests
+  app.use(express.static('client/build'));
+
+  //run index.html fils if it doesn't know the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
