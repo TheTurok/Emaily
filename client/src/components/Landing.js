410ee6d5a916506fe0b41df74748ed5f655589ec
Timothy Choi <TurokChoi@gmail.com>
2019-06-18 22:06:27 -0700

    instructions

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,8 +4,13 @@ const Landing = () => {
   return(
     <div style = {{textAlign: 'center'}}>
       <h3>
-        Collect feedback from you users
+        Collect feedback from you users answering surveys from their emails sent by you (:
       </h3>
+      <p>
+        To test out Emaily. Please Login with Google.
+        For Stripe Payment enter 4242424242424242 for card number and a valid expiration date with any number for the CVC.
+        Please keep emails to a minimum, only 100 emails can be sent a day with multiples people checking out my website. 
+      </p>
     </div>
   );
 }
