d32330ab17ced72e19f9e88d44db9ef14f72d17a
Timothy Choi <TurokChoi@gmail.com>
2019-11-05 21:34:16 -0800

    fixed stuff

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,9 +8,9 @@ const Landing = () => {
       </h3>
       <p>
         To test out Emaily. Please Login with Google.
-        <br>
+        <br></br>
         If already logged in please, press home button 'Emaily' to see survey list
-        <br>
+        <br></br>
         For Stripe Payment enter 4242424242424242 for card number and a valid expiration date with any number for the CVC.
         Please keep emails to a minimum, only 100 emails can be sent a day with multiples people checking out my website.
       </p>
